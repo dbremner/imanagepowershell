526d1038d1165490161ce23608459443d9291732
longneck <clongnecker@trenam.com>
2013-11-19 14:24:59 -0500

    get-imanworkspace, get-imandocument, export-imandocument and export-imanfolder work.

---
+++ iManagePowerShell/ExtensionMethods.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using iml = IManage;
+
+namespace iManagePowerShell
+{
+
+public static class ExtensionMethods
+{
+    public static string Replace(this string source, char[] find, string replacewith)
+    {
+        StringBuilder sb = new StringBuilder(source);
+        foreach (var f in find)
+            sb.Replace(f.ToString(), replacewith);
+        return sb.ToString();
+    }
+
+    public static List<iManSession> ToList(this iml.IManSessions source)
+    {
+        List<iManSession> results = new List<iManSession>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManSession((iml.IManSession)source.ItemByIndex(i)));
+        }
+        return results;
+    }
+
+    public static List<iManDocument> ToList(this iml.IManDocuments source, iManDatabase Database)
+    {
+        List<iManDocument> results = new List<iManDocument>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManDocument((iml.IManDocument)source.ItemByIndex(i), Database));
+        }
+        return results;
+    }
+
+    public static List<iManDatabase> ToList(this iml.IManDatabases source)
+    {
+        List<iManDatabase> results = new List<iManDatabase>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManDatabase((iml.IManDatabase)source.ItemByIndex(i)));
+        }
+        return results;
+    }
+
+    public static List<iManWorkspace> ToList(this iml.IManWorkspaces source, iManDatabase Database)
+    {
+        List<iManWorkspace> results = new List<iManWorkspace>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManWorkspace((iml.IManWorkspace)source.ItemByIndex(i), Database));
+        }
+        return results;
+    }
+
+    public static List<iManFolder> ToList(this iml.IManFolders source, iManDatabase Database)
+    {
+        List<iManFolder> results = new List<iManFolder>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManFolder((iml.IManFolder)source.ItemByIndex(i), Database));
+        }
+        return results;
+    }
+
+    public static List<iManWorkspace> ToWorkspaceList(this iml.IManFolders source, iManDatabase Database)
+    {
+        List<iManWorkspace> results = new List<iManWorkspace>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            //if (source.ObjectType.ObjectType == iml.imObjectType.imTypeWorkspace)
+                results.Add(new iManWorkspace((iml.IManWorkspace)source.ItemByIndex(i), Database));
+        }
+        return results;
+    }
+
+    public static List<iml.IManCustomAttribute> ToList(this iml.IManCustomAttributes source)
+    {
+        List<iml.IManCustomAttribute> results = new List<iml.IManCustomAttribute>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add((iml.IManCustomAttribute)source.ItemByIndex(i));
+        }
+        return results;
+    }
+
+    public static List<iManDocument> ToDocumentList(this iml.IManContents source, iManDatabase Database)
+    {
+        List<iManDocument> results = new List<iManDocument>(source.Count);
+        for (int i = 1; i <= source.Count; i++)
+        {
+            results.Add(new iManDocument((iml.IManDocument)source.ItemByIndex(i), Database));
+        }
+        return results;
+    }
+
+}
+
+}
--- iManagePowerShell/Objects.cs
+++ iManagePowerShell/Objects.cs
@@ -4,27 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
-using System.Management.Automation;
 using iml = IManage;
 
 namespace iManagePowerShell
 {
-    public class iManServer
+
+    public class iManServer : IDisposable
     {
-        private iml.IManDMS me;
+        public iml.IManDMS me;
 
-        private static Dictionary<string, iml.IManDMS> ServerList = new Dictionary<string, iml.IManDMS>();
+        private static Dictionary<string, iml.IManDMS> ServerSingletons = new Dictionary<string, iml.IManDMS>();
 
-        public iManServer(string Server)
+        public iManServer(string Server, string Username = null, string Password = null)
         {
             iml.IManDMS SingletonServer;
-            ServerList.TryGetValue(Server, out SingletonServer);
+            ServerSingletons.TryGetValue(Server, out SingletonServer);
             if (SingletonServer == null)
             {
                 me = new iml.ManDMS();
-                ServerList.Add(Server, me);
+                ServerSingletons.Add(Server, me);
                 iml.IManSession s = me.Sessions.Add(Server);
-                s.TrustedLogin();
+                if (Username == null)
+                {
+                    s.TrustedLogin();
+                }
+                else
+                {
+                    s.Login(Username, Password);
+                }
             }
             else
             {
@@ -32,21 +39,34 @@ namespace iManagePowerShell
             }
         }
 
-        public iManServer(string Server, string Username, string Password)
+        public void Dispose()
         {
-            iml.IManDMS SingletonServer;
-            ServerList.TryGetValue(Server, out SingletonServer);
-            if (SingletonServer == null)
+            foreach (iml.IManSession s in me.Sessions)
             {
-                me = new iml.ManDMS();
-                ServerList.Add(Server, me);
-                iml.IManSession s = me.Sessions.Add(Server);
-                s.Login(Username, Password);
+                if (s.Connected) s.Logout();
             }
-            else
+            me.Close();
+        }
+
+        public static List<iManServer> AutoConnect()
+        {
+            List<iManServer> result = new List<iManServer>();
+            Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Interwoven\WorkSite\8.0\Common\Login\RegisteredServers");
+            foreach (var s in rk.GetSubKeyNames().Select(s => rk.OpenSubKey(s)))
             {
-                me = SingletonServer;
+                string ServerName = s.GetValue("ServerName").ToString();
+                if (s.GetValue("TrustedLogin").ToString() == "Y")
+                {
+                    result.Add(new iManServer(ServerName));
+                }
+                else
+                {
+                    string username = s.GetValue("UserId").ToString();
+                    string password = s.GetValue("Password").ToString();
+                    result.Add(new iManServer(ServerName, username, password));
+                }
             }
+            return result;
         }
 
         public List<iManSession> Sessions
@@ -56,13 +76,18 @@ namespace iManagePowerShell
 
     }
 
-    public class iManSession
+    public class iManSession : IDisposable
     {
-        private iml.IManSession me;
+        public iml.IManSession me;
 
         public iManSession(iml.IManSession Session)
         {
             me = Session;
+        }
+
+        public void Dispose()
+        {
+            if (me.Connected) me.Logout();
         }
 
         public List<iManDatabase> Databases
@@ -82,14 +107,29 @@ namespace iManagePowerShell
         }
     }
 
-
     public class iManDatabase
     {
-        private iml.IManDatabase me;
+        public iml.IManDatabase me;
 
-        public iManDatabase(iml.IManDatabase database)
+        public iml.imProfileAttributeID _ClientCustomField;
+        public string ClientCustomField
+        {
+            get { return _ClientCustomField.ToString(); }
+            set { _ClientCustomField = (iml.imProfileAttributeID)Enum.Parse(typeof(iml.imProfileAttributeID), "imProfileCustom" + value); }
+        }
+
+        public iml.imProfileAttributeID _MatterCustomField;
+        public string MatterCustomField
+        {
+            get { return _MatterCustomField.ToString(); }
+            set { _MatterCustomField = (iml.imProfileAttributeID)Enum.Parse(typeof(iml.imProfileAttributeID), "imProfileCustom" + value); }
+        }
+
+        public iManDatabase(iml.IManDatabase database, int ClientCustomFieldPosition = 1, int MatterCustomFieldPosition = 2)
         {
             me = database;
+            ClientCustomField = ClientCustomFieldPosition.ToString();
+            MatterCustomField = MatterCustomFieldPosition.ToString();
         }
 
         public string Name
@@ -100,35 +140,117 @@ namespace iManagePowerShell
             }
         }
 
-        public List<iManWorkspace> SearchWorkspaces()
+        public List<iManWorkspace> SearchWorkspaces(iml.IManWorkspaceSearchParameters wssp, iml.IManProfileSearchParameters psp)
         {
-            return ((iml.IManWorkspaces)me.SearchWorkspaces(me.Session.DMS.CreateProfileSearchParameters(), me.Session.DMS.CreateWorkspaceSearchParameters())).ToList();
+            return ((iml.IManFolders)me.SearchWorkspaces(psp, wssp)).ToWorkspaceList(this);
         }
 
         public List<iManDocument> SearchDocuments()
         {
-            return ((iml.IManDocuments)me.SearchDocuments(me.Session.DMS.CreateProfileSearchParameters(), false)).ToList();
+            return ((iml.IManDocuments)me.SearchDocuments(me.Session.DMS.CreateProfileSearchParameters(), false)).ToList(this);
         }
+
+        public iManDocument GetDocument(int DocumentNumber, int DocumentVersion)
+        {
+            return new iManDocument(me.GetDocument(DocumentNumber, DocumentVersion), this);
+        }
+
+        public List<iManDocument> CheckedOutDocuments { get { return me.CheckedOutList.ToDocumentList(this); } }
+
     }
 
     public class iManWorkspace
     {
-        private iml.IManWorkspace me;
+        public iml.IManWorkspace me;
+        public iManDatabase Database { get; private set; }
 
-        public iManWorkspace(iml.IManWorkspace workspace)
+        public iManWorkspace(iml.IManWorkspace workspace, iManDatabase Database)
         {
             me = workspace;
+            this.Database = Database;
         }
+
+        public List<iManFolder> SubFolders
+        {
+            get
+            {
+                return me.SubFolders.ToList(Database);
+            }
+        }
+
+        public void Refile()
+        {
+            SubFolders.ForEach(f => f.Refile());
+        }
+
+        public string Name { get { return me.Name; } set { me.Name = value; } }
+
+        public string Description { get { return me.Description; } set { me.Description = value; } }
+
+        public void Update() { me.Update(); }
+
+    }
+
+    public class iManFolder
+    {
+        public iml.IManFolder me;
+        public iManDatabase Database { get; private set; }
+
+        public iManFolder(iml.IManFolder folder, iManDatabase Database)
+        {
+            me = folder;
+            this.Database = Database;
+        }
+
+        public string Name { get { return me.Name; } set { me.Name = value; } }
+
+        public List<iManDocument> Documents
+        {
+            get
+            {
+                List<iManDocument> results = new List<iManDocument>();
+                foreach (iml.IManContent c in me.Contents)
+                    if (c is iml.IManDocument)
+                        results.Add(new iManDocument((iml.IManDocument)c, Database));
+                return results;
+            }
+        }
+
+        public List<iManFolder> Folders { get { return me.SubFolders.ToList(Database); } }
+
+        public void Refile()
+        {
+        }
+
+        public void Update() { me.Update(); }
     }
 
     public class iManDocument
     {
-        private iml.IManDocument me;
+        public iml.IManDocument me;
+        public iManDatabase Database { get; private set; }
 
-        public iManDocument(iml.IManDocument document)
+        public iManDocument(iml.IManDocument Document, iManDatabase Database)
         {
-            me = document;
+            me = Document;
+            this.Database = Database;
         }
+
+        public int Number { get { return me.Number; } }
+
+        public int Version { get { return me.Version; } }
+
+        public bool CheckedOut { get { return me.CheckedOut; } }
+
+        public string Comment { get { return me.Comment; } set { me.Comment = value; } }
+
+        public string Description { get { return me.Description; } set { me.Description = value; } }
+
+        public string ClientCode { get { return me.CustomAttributes.ToList().Where(a => a.Type == Database._ClientCustomField).Single().Name; } }
+
+        public string MatterCode { get { return me.CustomAttributes.ToList().Where(a => a.Type == Database._MatterCustomField).Single().Name; } }
+
+        public string Extension { get { return me.Extension; } }
 
         public void CheckOut(string Filename)
         {
@@ -137,6 +259,9 @@ namespace iManagePowerShell
             me.CheckOut(Filename, iml.imCheckOutOptions.imDontReplaceExistingFile, DateTime.Now.AddDays(1), "");
         }
 
+        /// <summary>
+        /// If run from the same machine as CheckOut(), uploads the checked out local copy of the document and removes the checked out status.
+        /// </summary>
         public void CheckIn()
         {
             object errorObject = new object();
@@ -144,6 +269,9 @@ namespace iManagePowerShell
                 me.CheckIn(me.CheckoutLocation, iml.imCheckinDisposition.imCheckinReplaceOriginal, iml.imCheckinOptions.imDontKeepCheckedOut, ref errorObject);
         }
 
+        /// <summary>
+        /// Removes the checked out status of a checked out document.
+        /// </summary>
         public void Unlock()
         {
             if (me.CheckedOut)
@@ -152,50 +280,27 @@ namespace iManagePowerShell
                 throw new ApplicationException(String.Format("Document {0}.{1} not checked out", me.Number, me.Version));
 
         }
+
+        /// <summary>
+        /// Saves a copy of the document to the specified file without checking it out.
+        /// </summary>
+        /// <param name="filename">The path to the file to save to.</param>
+        /// <param name="ConvertToHTML">If true, converts the document to HTML during the copy.</param>
+        public void GetCopy(string filename, bool ConvertToHTML = false)
+        {
+            me.GetCopy(filename, ConvertToHTML ? iml.imGetCopyOptions.imHTMLFormat : iml.imGetCopyOptions.imNativeFormat);
+        }
+
+        public void Update() { me.Update(); }
+
+        /// <summary>
+        /// The folders that contain this document, a.k.a. "Where Used".
+        /// </summary>
+        public List<iManFolder> Folders { get { return me.Folders.ToList(Database); } }
+
+
+
     }
 
-    public static class ExtensionMethods
-    {
-        public static List<iManSession> ToList(this iml.IManSessions ds)
-        {
-            List<iManSession> results = new List<iManSession>(ds.Count);
-            for (int i = 1; i <= ds.Count; i++)
-            {
-                results.Add(new iManSession((iml.IManSession)ds.ItemByIndex(i)));
-            }
-            return results;
-        }
-
-        public static List<iManDocument> ToList(this iml.IManDocuments ds)
-        {
-            List<iManDocument> results = new List<iManDocument>(ds.Count);
-            for (int i = 1; i <= ds.Count; i++)
-            {
-                results.Add(new iManDocument((iml.IManDocument)ds.ItemByIndex(i)));
-            }
-            return results;
-        }
-
-        public static List<iManDatabase> ToList(this iml.IManDatabases ds)
-        {
-            List<iManDatabase> results = new List<iManDatabase>(ds.Count);
-            for (int i = 1; i <= ds.Count; i++)
-            {
-                results.Add(new iManDatabase((iml.IManDatabase)ds.ItemByIndex(i)));
-            }
-            return results;
-        }
-
-        public static List<iManWorkspace> ToList(this iml.IManWorkspaces ws)
-        {
-            List<iManWorkspace> results = new List<iManWorkspace>(ws.Count);
-            for (int i = 1; i <= ws.Count; i++)
-            {
-                results.Add(new iManWorkspace((iml.IManWorkspace)ws.ItemByIndex(i)));
-            }
-            return results;
-        }
-
-    }
 
 }
